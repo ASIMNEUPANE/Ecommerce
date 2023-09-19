2963a042ebabe65cd8982092625df1c8b4b78728
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-09-20 03:05:07 +0545

    add login features

--- server/modules/users/user.routes.js
+++ server/modules/users/user.routes.js
@@ -13,6 +13,19 @@ res.json({data:result, msg:"Succes"});
         next(e)
     }
 })
+router.post('/login', async(req,res,next)=>{
+    try{
+        const {email,password}= req.body
+        if (! email || !password) throw new Error("Email or Password is missing")
+result = await controller.login(email,password);
+res.json({data:result, msg:"Succes"});
+    }
+    catch(e){
+        next(e)
+    }
+})
+
+
 router.get('/', async(req,res,next)=>{
     try{
 result = await controller.list();
