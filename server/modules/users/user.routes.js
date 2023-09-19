0da9540246a5b7292c8e3cb37d2272e28cb82f34
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-09-20 02:07:05 +0545

    finalize crud

---
+++ server/modules/users/user.routes.js
@@ -0,0 +1,41 @@
+const router = require('express').Router();
+const controller = require('./user.controller')
+
+
+
+
+router.post('/', async(req,res,next)=>{
+    try{
+result = await controller.create(req.body);
+res.json({data:result, msg:"Succes"});
+    }
+    catch(e){
+        next(e)
+    }
+})
+router.get('/', async(req,res,next)=>{
+    try{
+result = await controller.list();
+res.json({data:result, msg:"Succes"});
+    }
+    catch(e){
+        next(e)
+    }
+})
+
+router.delete('/:id', async(req,res,next)=>{
+    try{
+result = await controller.deleteById(req.params);
+res.json({data:result, mssg: "Succes"});
+    }
+    catch(e){
+        next(e)
+    }
+})
+
+
+
+
+
+
+module.exports= router;
