193d2bf90633483170eb5c744afed7a52a2c5636
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-09-28 13:04:27 +0545

    created seed for user and product

--- server/database/seed.js
+++ server/database/seed.js
@@ -1,37 +1,97 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
-const Controller = require("../modules/users/user.controller");
+const productController = require("../modules/products/product.model");
+const userControllor = require('../modules/users/user.model')
 
 mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
 var setup = {
   initialize: async () => {
     await mongoose.connection.dropDatabase();
+
+
+
+
+
     console.log("DB reset");
-    console.log("creating admin user ");
-    const payload = {
-      name: "Asim Neupane",
-      email: "asimneupane11@gmail.com",
-      password: await bcrypt.hash("password", +process.env.SALT_ROUND),
+    console.log("Creating Admin user");
+    const payloads = [
+      {
+      name: "Raktim Admin",
+      email: "rakimsth@gmail.com",
+      password: await bcrypt.hash("12345", +process.env.SALT_ROUND),
       isEmailVerified: true,
-      isActive: true,
       roles: ["admin"],
-    };
-    await Controller.create(payload);
+    },
+      {
+      name: "Asim Admin",
+      email: "asimneupane11@gmail.com",
+      password: await bcrypt.hash("12345", +process.env.SALT_ROUND),
+      isEmailVerified: true,
+      roles: ["admin"],
+    }
+  ];
+    await userControllor.create(payloads);
+    console.log("---------DONE----------");
+
+    console.log("Creating Normal user");
+    const userPayload = [
+      {
+      name: "Raktim User",
+      email: "raktim@rumsan.com",
+      password: await bcrypt.hash("12345", +process.env.SALT_ROUND),
+      isEmailVerified: true,
+    },
+      {
+      name: "Asim User",
+      email: "asimn11@gmail.com",
+      password: await bcrypt.hash("12345", +process.env.SALT_ROUND),
+      isEmailVerified: true,
+    }
+  ];
+    await userControllor.create(userPayload);
+    console.log("Done");
+
+"--------------------------------------------------------------------------------------"
+
+
+    console.log("DB reset");
+    console.log("creating T-shirt  ");
+    const payload = [
+      {
+        product_name: "Armani tshirt",
+        product_description: "Best pants in the town",
+        product_type: "tshirt",
+        product_price: 2000,
+      },
+      {
+        product_name: "zara tshirt",
+        product_description: "Best pants in the town",
+        product_type: "tshirt",
+        product_price: 3000,
+      },
+    ];
+    await productController.create(payload);
 
     console.log("-----DONE-----");
 
     console.log("DB reset");
-    console.log("creating Normal user ");
-    const userPayload = {
-      name: "Asim User",
-      email: "asim11@gmail.com",
-      password: await bcrypt.hash("password", +process.env.SALT_ROUND),
-
-      isEmailVerified: true,
-      isActive: true,
-    };
-    await Controller.create(userPayload);
+    console.log("creating pants ");
+    const pantPayload = [
+      {
+        product_name: "H&M pants",
+        product_description: "Best pants in the town",
+        product_type: "pants",
+        product_price: 2500,
+      },
+      {
+        product_name: "gucci pants",
+        product_description: "world class pants",
+        product_type: "pants",
+        product_price: 5000,
+      },
+    ];
+    await productController.create(pantPayload);
     console.log("done");
   },
 };
--- server/utils/secure.js
+++ server/utils/secure.js
@@ -10,8 +10,9 @@ const secureAPI = (roles) => {
     
   return async (req, res, next) => {
     try{
-    const token = req.headers?.authorization;
-    if (!token) throw new Error("acces token require");
+    const token = req?.headers?.authorization;
+    console.log(token)
+    if (!token) throw new Error("Acces token require");
     const accesToken = token.split("Bearer ")[1];
     const { data } = verifyJWT(accesToken);
     if (!data) throw new Error("Data is not availabe");
