6515a995db8f749a326ba4e9059411099cbe7950
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-10-04 00:57:31 +0545

    new react vite setup env

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,5 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import Home from "./pages/home";
-import Cart from "./pages/cart";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/cart",
-    element: <Cart />,
-  },
-  {
-    path: "/",
-    element: <Home />,
-  },
-]);
-
 function App() {
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  );
+  return <>Helloworld</>;
 }
+
 export default App;
