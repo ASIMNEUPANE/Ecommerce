83fdabbd2e3faec139ea7ecc9b95237b9e9655e5
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-10-11 01:06:47 +0545

    create checkout

--- client/src/constant/index.jsx
+++ client/src/constant/index.jsx
@@ -1,10 +1,10 @@
 // export const SERVER_URL = process.env.REACT_APP_URL || "https://fakestoreapi.com "
-export const SERVER_URL =  "https://fakestoreapi.com ";
+export const SERVER_URL = "https://fakestoreapi.com";
 
-const version = "/api/v1"
-export const URLS ={
+console.log(SERVER_URL);
+const version = "/api/v1";
+export const URLS = {
+  // PRODUCTS : SERVER_URL+ "api/v1/products" for actual api call
 
-    // PRODUCTS : SERVER_URL+ "api/v1/products" for actual api call
-
-    PRODUCTS : version+'/products',
-}
+  PRODUCTS: version + "/products",
+};
