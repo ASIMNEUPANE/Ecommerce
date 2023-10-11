3f1f189cc592f0943049277cc3249903d310e57e
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-10-11 23:49:47 +0545

    checkout validation and assign

--- client/src/pages/Checkout.jsx
+++ client/src/pages/Checkout.jsx
@@ -4,10 +4,47 @@ import { useSelector } from "react-redux";
 
 export default function Checkout() {
   const { cart } = useSelector((state) => state.cart);
+  const [checkout, setCheckout] = useState({
+    name: "",
+    email: "",
+    address: "",
+    country: "",
+    state: "",
+    pobox: "",
+    amount: 0,
+    status: "",
+    products: [],
+    payment: [
+      {
+        nameOnCard: "",
+        cardNumber: "",
+        exp: "",
+        cvv: "",
+      },
+    ],
+    paymentMethod: "",
+  });
 
   const getTotal = () => {
     return cart.reduce((acc, obj) => acc + obj.price * obj.quantity, 0);
+     
   };
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    const payload = checkout
+    payload.amount = getTotal()
+    const  products = cart.map((item)=>{
+      return{
+        product: item?.id,
+        quantity: item?.quantity,
+        price: item?.price,
+        amount: Number(item?.quantity) * Number(item?.price),
+      };
+    })
+    payload.products = products;
+   
+  }
 
   const [payment, setPayment] = useState("COD");
   const [showPaymentForm, setShowPaymentForm] = useState(false);
@@ -22,7 +59,7 @@ export default function Checkout() {
       setShowPaymentForm(false);
     }
   };
-  
+
   return (
     <>
       <div className="row">
@@ -72,17 +109,23 @@ export default function Checkout() {
             </li>
           </ul>
         </div>
-        <div className="col-md-8 order-md-1">
+        <div className="col-md-8 order-md-0">
           <h4 className="mb-3">Billing address</h4>
           <form className="needs-validation" noValidate>
             <div className="row">
-              <div className="col-md-12 mb-3">
+              <div className="col-md-02 mb-3">
                 <label htmlFor="fullName">Full name</label>
                 <input
                   type="text"
                   className="form-control"
                   id="fullName"
                   placeholder="Asim Neupane"
+                  value={checkout?.name}
+                  onChange={(e) =>
+                    setCheckout((prev) => {
+                      return { ...prev, name: e.target.value };
+                    })
+                  }
                   required
                 />
                 <div className="invalid-feedback">
@@ -100,6 +143,12 @@ export default function Checkout() {
                 className="form-control"
                 id="email"
                 placeholder="you@example.com"
+                value={checkout?.email}
+                onChange={(e) =>
+                  setCheckout((prev) => {
+                    return { ...prev, email: e.target.value };
+                  })
+                }
               />
               <div className="invalid-feedback">
                 Please enter a valid email address for shipping updates.
@@ -112,8 +161,14 @@ export default function Checkout() {
                 type="text"
                 className="form-control"
                 id="address"
-                placeholder="1234 Main St"
+                placeholder="0234 Main St"
                 required
+                value={checkout?.address}
+                onChange={(e) =>
+                  setCheckout((prev) => {
+                    return { ...prev, address: e.target.value };
+                  })
+                }
               />
               <div className="invalid-feedback">
                 Please enter your shipping address.
@@ -123,7 +178,16 @@ export default function Checkout() {
             <div className="row">
               <div className="col-md-5 mb-3">
                 <label htmlFor="country">Country</label>
-                <select className="form-select" required>
+                <select
+                  className="form-select"
+                  required
+                  value={checkout?.country}
+                  onChange={(e) =>
+                    setCheckout((prev) => {
+                      return { ...prev, country: e.target.value };
+                    })
+                  }
+                >
                   <option value="">Choose...</option>
                   <option value="Nepal">Nepal</option>
                 </select>
@@ -133,7 +197,17 @@ export default function Checkout() {
               </div>
               <div className="col-md-4 mb-3">
                 <label htmlFor="state">State</label>
-                <select className="form-select" id="state" required>
+                <select
+                  className="form-select"
+                  id="state"
+                  required
+                  value={checkout?.state}
+                  onChange={(e) =>
+                    setCheckout((prev) => {
+                      return { ...prev, state: e.target.value };
+                    })
+                  }
+                >
                   <option value="">Choose...</option>
                   <option value="Bagmati">Bagmati</option>
                   <option value="Gandaki">Gandaki</option>
@@ -155,6 +229,12 @@ export default function Checkout() {
                   id="zip"
                   placeholder=""
                   required
+                  value={checkout?.pobox}
+                  onChange={(e) =>
+                    setCheckout((prev) => {
+                      return { ...prev, pobox: e.target.value };
+                    })
+                  }
                 />
                 <div className="invalid-feedback">Zip code required.</div>
               </div>
@@ -172,7 +252,7 @@ export default function Checkout() {
                 checked={payment === "COD"}
                 onChange={handlePaymentMethodChange}
               />
-              <label className="form-check-label" htmlFor="inlineRadio1">
+              <label className="form-check-label" htmlFor="inlineRadio0">
                 Cash on delivery
               </label>
             </div>
@@ -217,6 +297,18 @@ export default function Checkout() {
                       id="cc-name"
                       placeholder=""
                       required
+                      value={checkout.payment[0].nameOnCard}
+                      onChange={(e) =>
+                        setCheckout((prev) => ({
+                          ...prev,
+                          payment: [
+                            {
+                              ...prev.payment[0],
+                              nameOnCard: e.target.value,
+                            },
+                          ],
+                        }))
+                      }
                     />
                     <small className="text-muted">
                       Full name as displayed on card
@@ -228,11 +320,23 @@ export default function Checkout() {
                   <div className="col-md-6 mb-3">
                     <label htmlFor="cc-number">Credit card number</label>
                     <input
-                      type="text"
+                      type="number"
                       className="form-control"
                       id="cc-number"
                       placeholder=""
                       required
+                      value={checkout.payment[0].cardNumber}
+                      onChange={(e) =>
+                        setCheckout((prev) => ({
+                          ...prev,
+                          payment: [
+                            {
+                              ...prev.payment[0],
+                              cardNumber: e.target.value,
+                            },
+                          ],
+                        }))
+                      }
                     />
                     <div className="invalid-feedback">
                       Credit card number is required
@@ -243,12 +347,25 @@ export default function Checkout() {
                   <div className="col-md-3 mb-3">
                     <label htmlFor="cc-expiration">Expiration</label>
                     <input
-                      type="text"
+                      type="date"
                       className="form-control"
                       id="cc-expiration"
                       placeholder=""
                       required
+                      value={checkout.payment[0].exp}
+                      onChange={(e) =>
+                        setCheckout((prev) => ({
+                          ...prev,
+                          payment: [
+                            {
+                              ...prev.payment[0],
+                              exp: e.target.value,
+                            },
+                          ],
+                        }))
+                      }
                     />
+
                     <div className="invalid-feedback">
                       Expiration date required
                     </div>
@@ -256,11 +373,23 @@ export default function Checkout() {
                   <div className="col-md-3 mb-3">
                     <label htmlFor="cc-expiration">CVV</label>
                     <input
-                      type="text"
+                      type="number"
                       className="form-control"
                       id="cc-cvv"
                       placeholder=""
                       required
+                      value={checkout.payment[0].cvv}
+                      onChange={(e) =>
+                        setCheckout((prev) => ({
+                          ...prev,
+                          payment: [
+                            {
+                              ...prev.payment[0],
+                              cvv: e.target.value,
+                            },
+                          ],
+                        }))
+                      }
                     />
                     <div className="invalid-feedback">
                       Security code required
@@ -276,7 +405,7 @@ export default function Checkout() {
             <div className="d-grid gap-2">
               <button
                 className="btn btn-secondary btn-lg btn-block"
-                onClick={(e) => handleSubmit(e)}
+                onClick={(e)=> handleSubmit(e)}
               >
                 Continue to checkout
               </button>
