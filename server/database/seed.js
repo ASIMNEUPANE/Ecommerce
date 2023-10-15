1c6046e1efe855256eb497fe0f66f1eea6e3cb48
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-10-15 10:27:39 +0545

    clean up

--- server/database/seed.js
+++ server/database/seed.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const productController = require("../modules/products/product.model");
 const userControllor = require("../modules/users/user.model");
+const catControllor = require("../modules/categories/category.model");
 
 mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
 var setup = {
@@ -54,6 +55,21 @@ var setup = {
 
     ("--------------------------------------------------------------------------------------");
 
+    const catpayload = [
+      {name:"tshirt"},
+      {name:"pants"},
+    ]
+    await catControllor.create(catpayload);
+    
+       
+
+
+
+
+
+
+    ("------------------------------------------------------------------------------------------")
+
     console.log("DB reset");
     console.log("creating T-shirt  ");
     const payload = [
@@ -65,6 +81,7 @@ var setup = {
         price: 2000,
         isRelease: true,
         quantity: 3000,
+        category: catpayload. ,
       },
       {
         name: "zara tshirt",
@@ -73,6 +90,7 @@ var setup = {
         price: 3000,
         isRelease: true,
         quantity: 4000,
+        category:"652711b5fe64cef2bd196656"
       },
     ];
     await productController.create(payload);
@@ -89,6 +107,7 @@ var setup = {
         price: 2500,
         isRelease: true,
         quantity: 4000,
+        category: "652711d2fe64cef2bd19665a"
       },
       {
         name: "gucci pants",
@@ -97,6 +116,7 @@ var setup = {
         price: 5000,
         isRelease: true,
         quantity: 4000,
+        category: "652711d2fe64cef2bd19665a"
       },
     ];
     await productController.create(pantPayload);
--- server/index.js
+++ server/index.js
@@ -1,31 +1,63 @@
-require('dotenv').config()
-const cors = require('cors');
-const express = require('express');
-const mongoose = require('mongoose')
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+const mongoose = require("mongoose");
+const stripe = require("stripe")(
+  "sk_test_51O0G6BFVTdUImbSsHNBUBgrrV36yrnblLlnCA9ILi1lfHwDlnXqonPlaS5r7SbSgPnxen18LSV1eMjkAvcfU54yF002Ln1v4hc"
+);
+
 const DB_URL = process.env.DB_URL;
-const indexRouter = require('./routes')
+const indexRouter = require("./routes");
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(DB_URL).then(()=>{console.log('DataBase connected...')} )
+mongoose.connect(DB_URL).then(() => {
+  console.log("DataBase connected...");
+});
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-
-
-app.use('/', indexRouter)
-
-
-app.use((err,req,res,next)=>{
- const errMsg = err? err.toString(): "something went wrong"
- res.status(500).json({data:"", msg: errMsg})
-});
-
-app.listen(PORT, ()=>{
-  console.log(`app is running on ${PORT}`)
+app.post("/create-intent", async (req,res)=>{
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount:1099,
+    currency:"usd",
+  });
+  res.json({client_secret: paymentIntent.client_secret})
 })
 
+app.post("/create-checkout-session", async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+     line_items:
+     [
+    //   {
+    //     price_data: {
+    //       currency: "usd",
+    //       product_data: {
+    //         name: "T-shirt",
+    //       },
+    //       unit_amount: 2000,
+    //     },
+    //     quantity: 1,
+    //   },
+     ],
+    mode: "payment",
+    success_url: "http://localhost:5173/checkout/success",
+    cancel_url: "http://localhost:5173/checkout/failed",
+  });
 
+  res.json({session, mssg:'success'});
+});
+
+app.use("/", indexRouter);
+
+app.use((err, req, res, next) => {
+  const errMsg = err ? err.toString() : "something went wrong";
+  res.status(500).json({ data: "", msg: errMsg });
+});
+
+app.listen(PORT, () => {
+  console.log(`app is running on ${PORT}`);
+});
