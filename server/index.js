b8c036a049f6040d2fbd9e3b282e3f744901dc7b
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-10-16 15:15:25 +0545

    payment exclusive project

--- server/index.js
+++ server/index.js
@@ -32,23 +32,23 @@ app.post("/create-checkout-session", async (req, res) => {
   const session = await stripe.checkout.sessions.create({
      line_items:
      [
-    //   {
-    //     price_data: {
-    //       currency: "usd",
-    //       product_data: {
-    //         name: "T-shirt",
-    //       },
-    //       unit_amount: 2000,
-    //     },
-    //     quantity: 1,
-    //   },
-     ],
+      {
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: 'T-shirt',
+          },
+          unit_amount: 2000,
+        },
+        quantity: 1,
+      },
+    ],
     mode: "payment",
     success_url: "http://localhost:5173/checkout/success",
     cancel_url: "http://localhost:5173/checkout/failed",
   });
 
-  res.json({session, mssg:'success'});
+  res.json({data:session?.url , mssg:'success'});
 });
 
 app.use("/", indexRouter);
