25d65bc5347cf7134100e534a79bc19c70949db4
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-10-18 14:39:07 +0545

    update products model

--- server/modules/products/product.model.js
+++ server/modules/products/product.model.js
@@ -3,25 +3,13 @@ const { ObjectId } = Schema.Types;
 const { commonSchema } = require("../../utils/commonSchema");
 
 const productSchema = new Schema({
-  name: { type: String, required: "Product name is required" },
-  alias:[{ type: String, required: true, unique: true }], 
-  description: {
-    type: String,
-    required: "Product description is required",
-    maxLength: 250,
-  },
- 
-  quantity: { type: Number, required: true, minLength: 1, maxLength: 100000 },
-  category: { type: ObjectId, ref: "Category" },
-
-  price: {
-    type: Number,
-    required: "Product price is required",
-    minLength: 1,
-    maxLength: 4,
-  },
-  images: [{ type:String}],
-  isRelease: { type: Boolean, default: false },
+  name: { type: String, required: true },
+  alias: [{ type: String, required: true, unique: true }],
+  description: { type: String },
+  quantity: { type: Number, required: true },
+  price: { type: Number, required: true },
+  category: { type: ObjectId, ref: "Category", required: true },
+  images: [{ type: String }],
   ...commonSchema,
 });
 
