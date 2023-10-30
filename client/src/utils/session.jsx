e7d6df183172ec0c0f82b9db9d6e6ade4894af27
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-10-30 18:35:01 +0545

    store token in session

---
+++ client/src/utils/session.jsx
@@ -0,0 +1,10 @@
+export const setToken = (token) => {
+    const saveToken = localStorage.setItem("access_token", token);
+    if (!saveToken) return null;
+    return true;
+  };
+  
+  export const getToken = () => {
+    return localStorage.getItem("access_token");
+  };
+  
