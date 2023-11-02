ef6c4ec1a064b791f4a1cadcd0d55030a2144eb1
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-11-03 02:29:46 +0545

    admin product pages

--- client/src/pages/admin/Product.jsx
+++ client/src/pages/admin/Product.jsx
@@ -1,9 +1,32 @@
 import Tables from "../../components/Table";
-
+import {  useDispatch, useSelector } from "react-redux";
+import { useCallback, useEffect } from "react";
+import {fetchProducts} from "../../slices/productSlice";
+import { Link } from "react-router-dom";
+// import AddProduct from "./AddProduct";
 function AdminProducts() {
+  const { products, loading, limit, total,page, currentPage } = useSelector(
+    (state) => state.products
+  );
+  const dispatch = useDispatch();
+  const initFetch = useCallback(() => {
+    dispatch(fetchProducts({limit,page,currentPage}))
+  }, [dispatch,currentPage,limit]);
+
+  const headers = ["ID","Name", "Quantity", "Price"]
+
+   useEffect(()=>{
+    initFetch()
+  },[initFetch])
   return (
     <>
-      <Tables />
+    <div className=" mb-2 flex d-flex justify-content-end">
+      <Link to='/admin/products/add'> <button className=" btn btn-danger">Add product</button></Link>
+   
+
+    </div>
+    {/* <AddProduct/> */}
+      <Tables data={products} headers={headers} />
     </>
   );
 }
