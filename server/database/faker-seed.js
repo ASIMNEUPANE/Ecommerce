1547b8528f46fc3a84801f97674cd849d727f497
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-11-08 01:37:03 +0545

    remove img from faker

--- server/database/faker-seed.js
+++ server/database/faker-seed.js
@@ -3,12 +3,13 @@ const { faker } = require("@faker-js/faker");
 const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
 const Controller = require("../modules/users/user.controller");
-const categoryController = require("../modules/categories/category.controller");
-const productController = require("../modules/products/product.controller");
+// const categoryController = require("../modules/categories/category.controller");
+// const productController = require("../modules/products/product.controller");
 
 mongoose.connect(process.env.DB_URL);
 var setup = {
   initialize: async () => {
+    
     await mongoose.connection.dropDatabase();
     console.log("DB reset");
     console.log("Creating Admin user");
@@ -32,33 +33,33 @@ var setup = {
     };
     await Controller.create(userPayload);
     console.log("---------DONE----------");
-    console.log("Creating categories");
-    const cat1 = await categoryController.create({ name: "Tshirt" });
-    const cat2 = await categoryController.create({ name: "Jeans" });
+    // console.log("Creating categories");
+    // const cat1 = await categoryController.create({ name: "Tshirt" });
+    // const cat2 = await categoryController.create({ name: "Jeans" });
 
-    console.log("Done");
-    console.log("-----Creating Products-----");
+    // console.log("Done");
+    // console.log("-----Creating Products-----");
 
-    const productCount = 100;
-    for (let i = 0; i < productCount; i++) {
+    // const productCount = 100;
+    // for (let i = 0; i < productCount; i++) {
       
-      await productController.create({
-        name: faker.commerce.productName({ category: 't-shirts' }),
-        alias: [],
-        description: faker.commerce.productDescription({ category: 't-shirts' }),
-        quantity: faker.commerce.price({ min: 0, max: 10, dec: 0 }),
-        price: faker.commerce.price({ min: 100, max: 2000, dec: 0 }),
-        category:
-          faker.number.binary({ min: 0, max: 1 }) === 0 ? cat1?._id : cat2?._id,
-        images: [
-          faker.image.urlLoremFlickr({ category: 't-shirts' }),
-          faker.image.urlLoremFlickr({ category: 't-shirts' }),
-          faker.image.urlLoremFlickr({ category: 't-shirts' }),
-        ],
-      });
-    }
+    //   await productController.create({
+    //     name: faker.commerce.productName({ category: 't-shirts' }),
+    //     alias: [],
+    //     description: faker.commerce.productDescription({ category: 't-shirts' }),
+    //     quantity: faker.commerce.price({ min: 0, max: 10, dec: 0 }),
+    //     price: faker.commerce.price({ min: 100, max: 2000, dec: 0 }),
+    //     category:
+    //       faker.number.binary({ min: 0, max: 1 }) === 0 ? cat1?._id : cat2?._id,
+    //     images: [
+    //       faker.image.urlLoremFlickr({ category: 't-shirts' }),
+    //       faker.image.urlLoremFlickr({ category: 't-shirts' }),
+    //       faker.image.urlLoremFlickr({ category: 't-shirts' }),
+    //     ],
+    //   });
+    // }
 
-    console.log("-----Creating Products Done-----");
+    // console.log("-----Creating Products Done-----");
   },
 };
 setup.initialize();
