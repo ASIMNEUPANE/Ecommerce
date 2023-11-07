e30c1835abdcad7dba795371c87d8a2f989adbd7
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-11-07 13:34:38 +0545

    edit page for category

--- client/src/pages/admin/categories/Edit.jsx
+++ client/src/pages/admin/categories/Edit.jsx
@@ -1,6 +1,76 @@
+import { useEffect, useState, useCallback } from "react";
+import { Link, useNavigate,useParams } from "react-router-dom";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import { useCategories } from "../../../hooks/useCategories";
 
 export default function Edit() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const { getById, updateById } = useCategories();
+  const [name, setName] = useState("");
+  const [slug, setSlug] = useState("");
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      await updateById(id, { name: name });
+      navigate("/admin/categories");
+    } catch (e) {
+      alert(e)
+    }
+  };
+
+  const fetchDetails = useCallback(async () => {
+    const result = await getById(id);
+
+    setName(result?.name);
+    setSlug(result?.slug);
+    setName({ result });
+  }, [id, getById]);
+
+  useEffect(() => {
+    fetchDetails();
+  }, [fetchDetails]);
+
   return (
-    <div>Edit</div>
-  )
+    <Container>
+      <Row>
+        <Col md={{ span: 4, offset: 4 }}>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="name"
+                placeholder="Enter Category name"
+                value={name}
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Slug</Form.Label>
+              <Form.Control
+                type="name"
+                placeholder="slug"
+                value={slug}
+                disabled
+              />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Update
+            </Button>
+            <Link
+              to="/admin/categories"
+              className="btn text-white  m-2 btn-danger "
+            >
+              Go Back
+            </Link>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
