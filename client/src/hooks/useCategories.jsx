caaf719528306b999eaff347a7a9494ab3125fa0
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-11-07 13:33:45 +0545

    create hooks for category for CRUD

---
+++ client/src/hooks/useCategories.jsx
@@ -0,0 +1,98 @@
+import { useState, useCallback } from "react";
+import API from "../utils/API";
+import { URLS } from "../constants";
+
+export const useCategories = () => {
+  const [data, setData] = useState([]);
+  const [category,setCategory]= useState({})
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [msg, setMsg] = useState("");
+
+  const create = async (payload) => {
+    try {
+      setLoading(true);
+      const { data } = await API.post(URLS.CATEGORIES, payload);
+      setData(data?.data?.data);
+
+      setMsg("Categories added Successfully");
+    } catch (e) {
+      const setErr = e.response ? e.response.data.msg : "Something went wrong";
+      setError(setErr);
+      throw setErr;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const list = useCallback(async () => {
+    try {
+      setLoading(true);
+      const { data } = await API.get(URLS.CATEGORIES);
+      setData(data?.data?.data);
+      setMsg("Categories fetch Successfully");
+    } catch (e) {
+      const setErr = e.response ? e.response.data.msg : "Something went wrong";
+      setError(setErr);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  const getById =useCallback( async (id) => {
+    try {
+      setLoading(true);
+      const { data } = await API.get(`${URLS.CATEGORIES}/${id}`);
+      setCategory(data?.data);
+      setMsg("Categories fetch Successfully");
+      return data.data;
+    } catch (e) {
+      const setErr = e.response ? e.response.data.msg : "Something went wrong";
+      setError(setErr);
+      throw setErr;
+    } finally {
+      setLoading(false);
+    }
+  },[]);
+
+  const updateById = async (id,payload) => {
+    try {
+      setLoading(true)
+      const result= await API.put(`${URLS.CATEGORIES}/${id}`,payload)
+      return result
+      
+    } catch (e) {
+      const setErr = e.response ? e.response.data.msg : "Something went wrong";
+      setError(setErr);
+    } finally {
+      setLoading(false)
+    }
+  };
+  const deleteById = async (id) => {
+    try {
+      setLoading(true);
+      const result = await API.delete(`${URLS.CATEGORIES}/${id}`);
+
+      return result;
+    } catch (e) {
+      const setErr = e.response ? e.response.data.msg : "Something went wrong";
+      setError(setErr);
+      throw setErr;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {
+    data,
+    msg,
+    error,
+    loading,
+    category,
+    list,
+    create,
+    getById,
+    updateById,
+    deleteById,
+  };
+};
