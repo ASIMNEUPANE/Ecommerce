db33c8870d990a3227f98f1b15abb3ee56b64fe1
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-11-09 00:15:37 +0545

    added user Url

--- client/src/constants/index.jsx
+++ client/src/constants/index.jsx
@@ -3,6 +3,7 @@ export const SERVER_URL = "http://localhost:3333";
 const version = "/api/v1";
 export const URLS = {
   AUTH: version + "/auth",
+  USERS: version + "/users",
   ORDERS: version + "/orders",
   CATEGORIES: version + "/categories",
   PRODUCTS: version + "/products",
