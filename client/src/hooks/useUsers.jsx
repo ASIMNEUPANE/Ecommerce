a9508a92180443ec9d7baf8b838bb638e9e1c109
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-11-09 00:16:40 +0545

    create a hook for user

---
+++ client/src/hooks/useUsers.jsx
@@ -0,0 +1,115 @@
+import { useState, useCallback } from "react";
+import API from "../utils/API";
+import { URLS } from "../constants";
+
+export const useUsers = () => {
+  const [data, setData] = useState([]);
+
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [msg, setMsg] = useState("");
+  console.log(data, "ajahia");
+
+  const create = async (payload) => {
+    try {
+      setLoading(true);
+      const { data } = await API.post(URLS.USERS, payload);
+      setData(data?.data?.data);
+
+      setMsg("Users added Successfully");
+    } catch (e) {
+      const setErr = e.response ? e.response.data.msg : "Something went wrong";
+      setError(setErr);
+      throw setErr;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const list = useCallback(async () => {
+    try {
+      setLoading(true);
+      const { data } = await API.get(URLS.USERS);
+      setMsg("Users fetch Successfully");
+      setData(data?.data?.data);
+      return data.data;
+    } catch (e) {
+      const setErr = e.response ? e.response.data.msg : "Something went wrong";
+      setError(setErr);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  const getById = useCallback(async (id) => {
+    try {
+      setLoading(true);
+      const { data } = await API.get(`${URLS.USERS}/${id}`);
+      setMsg("Users fetch Successfully");
+      return data.data;
+    } catch (e) {
+      const setErr = e.response ? e.response.data.msg : "Something went wrong";
+      setError(setErr);
+      throw setErr;
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  const updateById = async (id, payload) => {
+    try {
+      setLoading(true);
+      const result = await API.put(`${URLS.USERS}/${id}`, payload);
+      return result;
+    } catch (e) {
+      const setErr = e.response ? e.response.data.msg : "Something went wrong";
+      setError(setErr);
+    } finally {
+      setLoading(false);
+    }
+  };
+  const deleteById = async (id,payload) => {
+    try {
+        console.log({payload})
+      setLoading(true);
+      const result = await API.delete(`${URLS.USERS}/${id}`,{data:payload});
+
+      return result;
+    } catch (e) {
+      const setErr = e.response ? e.response.data.msg : "Something went wrong";
+      setError(setErr);
+      throw setErr;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const blockUser = async (id, payload) => {
+    //   {isActive : true}
+    try {
+      setLoading(true);
+      const { data } = await API.patch(`${URLS.USERS}/status/${id}`, payload);
+      setMsg("User Block successfully");
+      return data.data;
+    } catch (e) {
+      const setErr = e.response ? e.response.data.msg : "Something went wrong";
+      setError(setErr);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {
+    data,
+    msg,
+    error,
+    loading,
+    
+    blockUser,
+    list,
+    create,
+    getById,
+    updateById,
+    deleteById,
+  };
+};
