5a9b6d37223456cafef528f6944fc21a71731913
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-11-09 15:56:11 +0545

    added order features

--- client/src/pages/admin/orders/Add.jsx
+++ client/src/pages/admin/orders/Add.jsx
@@ -1,60 +1,89 @@
+import { useCallback, useEffect, useState } from "react";
+import { BsTrash } from "react-icons/bs";
+import {
+  Button,
+  Container,
+  Row,
+  Table,
+  Col,
+  Form,
+  Stack,
+} from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
-
-import { useEffect, useState, useCallback } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { Link, useNavigate, useParams } from "react-router-dom";
-import { Button, Col, Container, Form, Row } from "react-bootstrap";
-import { create } from "../../../services/orders";
 import { fetchProducts } from "../../../slices/productSlice";
+import { useOrder } from "../../../hooks/useOrder";
 
-export default function Add() {
+const Add = () => {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { id } = useParams();
+  const { create } = useOrder();
+  const { products: allProducts } = useSelector((state) => state.products);
 
-  const { products, loading, limit, total, currentPage } = useSelector(
-    (state) => state.products
-  );
-
-  const [selectedProduct, setSelectedProduct] = useState(null);
-
-  const [payload, setPayload] = useState({
+  const [order, setOrder] = useState({
     name: "",
     email: "",
-    paymentMethod: "Stripe",
-    amount: 0,
     address: "",
-    products: [{ _id: "", quantity: 0, price: 0, amount: 0 }],
-    orderId: "",
-    id: id || "",
-    status: "pending",
+    paymentMethod: "",
+    amount: 0,
   });
- 
-  useEffect(() => {
-    if (selectedProduct) {
-      const quantity = payload.products[0].quantity;
-      const price = selectedProduct.price;
-      setPayload((prev) => ({
-        ...prev,
-        amount: quantity * price,
-      }));
-    }
-  }, [selectedProduct, payload.products]);
+  const [item, setItem] = useState({
+    product: "",
+    quantity: 0,
+    price: 0,
+    amount: 0,
+  });
+  const [products, setProducts] = useState([]);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleSubmit = async (evnt) => {
     try {
-      await create(payload);
-      navigate("/admin/orders");
+      evnt.preventDefault();
+      const payload = order;
+      payload.products = products;
+      const result = await create(payload);
+      if (result.msg === "success") {
+        alert("Order created successfully");
+        navigate("/admin/orders");
+      }
     } catch (e) {
       alert(e);
     }
   };
 
-  const dispatch = useDispatch();
+  const handleAddingProducts = (e) => {
+    e.preventDefault();
+    if (!item || !item.product || !item?.quantity) return;
+    const newProduct = [...products, item];
+    setProducts(newProduct);
+    setOrder((prev) => {
+      return {
+        ...prev,
+        amount: newProduct.reduce((acc, item) => acc + item?.amount, 0),
+      };
+    });
+    setItem({
+      product: "",
+      quantity: 0,
+      price: 0,
+      amount: 0,
+    });
+  };
+
+  const removeProduct = (id) => {
+    const newProduct = products.filter((product) => product.product !== id);
+    setProducts(newProduct);
+    setOrder((prev) => {
+      return {
+        ...prev,
+        amount: newProduct.reduce((acc, item) => acc + item?.amount, 0),
+      };
+    });
+  };
 
   const initFetch = useCallback(() => {
-    dispatch(fetchProducts({ limit, page: currentPage }));
-  }, [dispatch, limit, currentPage]);
+    dispatch(fetchProducts({ limit: 10000, page: 1 }));
+  }, [dispatch]);
 
   useEffect(() => {
     initFetch();
@@ -63,156 +92,193 @@ export default function Add() {
   return (
     <Container>
       <Row>
-        <Col>
-          <h3 className="text-center">Order Number# {payload.orderId}</h3>
+        <h1 className="text-center">Add new Order</h1>
+        <Col md={3} className="m-2">
           <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3">
               <Form.Label>Name</Form.Label>
               <Form.Control
                 type="text"
-                placeholder="Enter order name"
-                value={payload.name}
-                onChange={(e) =>
-                  setPayload({ ...payload, name: e.target.value })
-                }
+                placeholder="Enter Buyer Name"
+                value={order?.name}
+                onChange={(e) => {
+                  setOrder((prev) => {
+                    return { ...prev, name: e.target.value };
+                  });
+                }}
               />
             </Form.Group>
-
             <Form.Group className="mb-3">
               <Form.Label>Email</Form.Label>
               <Form.Control
-                type="text"
-                placeholder="Enter Email"
-                value={payload.email}
-                onChange={(e) =>
-                  setPayload({ ...payload, email: e.target.value })
-                }
+                type="email"
+                placeholder="Enter Buyer Email"
+                value={order?.email}
+                onChange={(e) => {
+                  setOrder((prev) => {
+                    return { ...prev, email: e.target.value };
+                  });
+                }}
               />
             </Form.Group>
-
             <Form.Group className="mb-3">
               <Form.Label>Address</Form.Label>
               <Form.Control
                 type="text"
-                placeholder="Enter your address"
-                value={payload.address}
-                onChange={(e) =>
-                  setPayload({ ...payload, address: e.target.value })
-                }
+                placeholder="Enter Address"
+                value={order?.address}
+                onChange={(e) => {
+                  setOrder((prev) => {
+                    return { ...prev, address: e.target.value };
+                  });
+                }}
               />
             </Form.Group>
-
             <Form.Group className="mb-3">
               <Form.Label>Payment Method</Form.Label>
               <Form.Select
-                value={payload.paymentMethod}
-                onChange={(e) =>
-                  setPayload({ ...payload, paymentMethod: e.target.value })
-                }
+                value={order?.paymentMethod}
+                onChange={(e) => {
+                  setOrder((prev) => {
+                    return { ...prev, paymentMethod: e.target.value };
+                  });
+                }}
               >
-                <option value="Stripe">Stripe</option>
+                <option>Open this select menu</option>
                 <option value="COD">COD</option>
+                <option value="STRIPE">STRIPE</option>
               </Form.Select>
             </Form.Group>
-
-            <Form.Group className="mb-3">
-              <Form.Label>Status</Form.Label>
-              <Form.Select
-                value={payload.status}
-                onChange={(e) =>
-                  setPayload({ ...payload, status: e.target.value })
-                }
-              >
-                <option value="pending">Pending</option>
-                <option value="completed">Complete</option>
-                <option value="failed">Failed</option>
-              </Form.Select>
-            </Form.Group>
-
             <Form.Group className="mb-3">
               <Form.Label>Amount</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Amount"
-                disabled
-                value={payload.amount}
-              />
+              <Form.Control type="text" disabled value={order?.amount} />
             </Form.Group>
-
-            <Button variant="primary" type="submit">
-              Update
-            </Button>
-            <Link to="/admin/orders" className="btn text-white m-2 btn-danger">
-              Go Back
-            </Link>
+            <Stack gap={1} direction="horizontal">
+              <Button
+                variant="primary"
+                type="submit"
+                size="md"
+                style={{ width: "8rem" }}
+              >
+                Add
+              </Button>
+              <Link to="/admin/orders" className="btn btn-danger">
+                Go Back
+              </Link>
+            </Stack>
           </Form>
         </Col>
-
-        <Col>
-          <Form.Group className="mb-3">
-            <Form.Label>Products</Form.Label>
-            <Form.Select onChange={(e)=> {
-               const selectedProduct = products.find(
-                (product) => product._id === e.target.value
-              );
-          
-              setSelectedProduct(selectedProduct);
-            }}>
-              {products && products.length > 0
-                ? products.map((product) => (
-                    <option key={product._id} value={product._id}>
-                      {product.name}
-                    </option>
-                  ))
-                : null}
-            </Form.Select>
-          </Form.Group>
-
-          {selectedProduct && (
-            <Form.Group className="mb-3">
-              <Form.Label>Quantity</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter quantity"
-                value={payload.products[0].quantity}
-                onChange={(e) =>
-                  setPayload((prev) => ({
-                    ...prev,
-                    products: [
-                      {
-                        ...prev.products[0],
-                        quantity: e.target.value,
-                      },
-                    ],
-                  }))
-                }
-              />
-            </Form.Group>
-          )}
-
-          {selectedProduct && (
-            <Form.Group className="mb-3">
-              <Form.Label>Price</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter price"
-                value={payload.products[0].price}
-                onChange={(e) =>
-                  setPayload((prev) => ({
-                    ...prev,
-                    products: [
-                      {
-                        ...prev.products[0],
-                        price: e.target.value,
-                      },
-                    ],
-                  }))
-                }
-              />
-            </Form.Group>
-          )}
+        <Col className="border rounded">
+          <h5 className="text-center">Add Products</h5>
+          <Row>
+            <Col lg={4}>
+              <Form onSubmit={handleAddingProducts}>
+                <Form.Group className="mb-3">
+                  <Form.Label>Product</Form.Label>
+                  <Form.Select
+                    value={item?.product}
+                    onChange={(e) => {
+                      setItem((prev) => {
+                        return {
+                          ...prev,
+                          product: e.target.value,
+                          price: allProducts.find(
+                            (item) => item._id === e.target.value
+                          ).price,
+                        };
+                      });
+                    }}
+                  >
+                    <option>Open this select menu</option>
+                    {allProducts.map((prod) => {
+                      return (
+                        <option key={prod?._id} value={prod?._id}>
+                          {prod?.name}
+                        </option>
+                      );
+                    })}
+                  </Form.Select>
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Label>Quantity</Form.Label>
+                  <Form.Control
+                    type="number"
+                    placeholder="Enter Quantity"
+                    value={item?.quantity}
+                    onChange={(e) => {
+                      setItem((prev) => {
+                        return {
+                          ...prev,
+                          quantity: Number(e.target.value),
+                          amount: Number(e.target.value) * item?.price,
+                        };
+                      });
+                    }}
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Label>Price</Form.Label>
+                  <Form.Control type="number" disabled value={item?.price} />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Label>Amount</Form.Label>
+                  <Form.Control
+                    type="number"
+                    disabled
+                    placeholder="Enter Quantity"
+                    value={item?.amount}
+                  />
+                </Form.Group>
+                <Stack gap={1} direction="horizontal">
+                  <Button type="submit">Add Product</Button>
+                </Stack>
+              </Form>
+            </Col>
+            <Col className="m-2">
+              <Table striped bordered hover>
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Price</th>
+                    <th>Quantity</th>
+                    <th>Amount</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {products &&
+                    products.map((product, idx) => {
+                      return (
+                        <tr key={`${product?.product}${idx}`}>
+                          <td>{idx + 1}</td>
+                          <td style={{ width: "40%" }}>
+                            {
+                              allProducts.find(
+                                (item) => item?._id === product?.product
+                              ).name
+                            }
+                          </td>
+                          <td>{product?.price}</td>
+                          <td>{product?.quantity}</td>
+                          <td>{product?.amount}</td>
+                          <td className="text-center">
+                            <BsTrash
+                              color="red"
+                              onClick={() => removeProduct(product?.product)}
+                            />
+                          </td>
+                        </tr>
+                      );
+                    })}
+                </tbody>
+              </Table>
+            </Col>
+          </Row>
         </Col>
       </Row>
     </Container>
   );
-}
+};
+
+export default Add;
