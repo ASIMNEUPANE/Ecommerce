e1cdf98f0830b370743b8003dfdfa5d8c9bc4414
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-12-01 22:59:40 +0545

    Update Vite config and main script

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
   host: '127.0.0.1',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  }
 });
