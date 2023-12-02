01ac0e5ab4e08a5c2aed150878497e51b1366dec
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-12-02 12:20:40 +0545

    Update base URL and file paths

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/gadgethub/",
   host: '127.0.0.1',
   plugins: [react()],
   build: {
