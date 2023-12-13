b0b15d6ebdfc7096112015bf7adb4c5706d9aabe
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-12-13 18:48:02 +0545

    Remove commented code from vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-// import { resolve } from 'path'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,12 +8,6 @@ export default defineConfig({
   base: "/",
   host: '127.0.0.1',
   plugins: [react()],
-  // build: {
-  //   rollupOptions: {
-  //     input: {
-  //       main: resolve(__dirname, 'index.html'),
-  //     },
-  //   },
-  // }
+  
 });
 
