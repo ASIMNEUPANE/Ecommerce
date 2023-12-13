ec1563f83e97d2fb40198c9c75d93d860293071e
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-12-13 19:06:52 +0545

    Update Vite config to use hash mode and set output directory to "docs"

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
-  host: "127.0.0.1",
+  mode: 'hash', 
   plugins: [react()],
+  build: {
+    outDir: "docs",
+  },
 });
