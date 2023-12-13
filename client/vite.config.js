5970627423c76ccb0bcc270e2072261371530674
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-12-13 19:15:55 +0545

    Update package.json and vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,10 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
-  mode: 'hash', 
+  base: "", 
   plugins: [react()],
-  build: {
-    outDir: "dist",
-  },
+  
 });
