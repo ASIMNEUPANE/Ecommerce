c9481cca8fd6e6d0003ed87f3bd1f7e32d9edd3b
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-12-13 18:51:37 +0545

    test

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  
   base: "/",
-  host: '127.0.0.1',
+  host: "127.0.0.1",
   plugins: [react()],
-  
 });
-
